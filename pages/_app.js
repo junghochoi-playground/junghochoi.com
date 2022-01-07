c50cc3b8a8716a7cf8823febf81b2108330607ae
junghochoi <dchoi315@gmail.com>
2022-01-07 17:46:26 -0500

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
 
-
-
+import Layout from '../components/layouts/main'
 import theme from '../lib/theme'
-function MyApp({ Component, pageProps }) {
+function Website({ Component, pageProps, router  }) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+
+      <Layout router={router}>
+        <Component {...pageProps} />
+      </Layout>
+      
     </ChakraProvider>
   );
 }
 
-export default MyApp;
+export default Website;
--- pages/index.js
+++ pages/index.js
@@ -11,22 +11,17 @@ import {
 
 export default function Index() {
   return (
-    <Container borderColor='red.500' borderWidth='1px'>
+    <Container maxW={'container.md'} borderColor='red.500' borderWidth='1px'>
       <Flex justify='space-between' align='center' my={2}>
         <Box>
           <Heading
             as='h1'
-            // size={{ base: '2xl', md: '3xl' }}
-            // textAlign='center'
             variant='page-title'
           >
             Jungho Daniel Choi
           </Heading>
         </Box>
 
-        {/* <Text fontSize={{ base: '24px', md: '40px', lg: '56px' }}>
-          This is responsive text
-        </Text> */}
         <Image
           ml={3}
           boxSize='175px'
